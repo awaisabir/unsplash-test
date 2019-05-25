f687e7f83653eca0c94482e1884863133640333a
awaisabir <aqureshi@klipfolio.com>
2019-05-25 13:15:18 -0400

    initial setup for app

---
+++ src/config/Unsplash.default.js
@@ -0,0 +1,8 @@
+/* 
+    Rename file to Unsplash.js with your respective keys
+*/
+
+export default {
+    ACCESS_KEY: "<place Unsplash access key here>",
+    SECRET_KEY: "<place Unsplash secret key here>"
+};
