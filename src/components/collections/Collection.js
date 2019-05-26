d19276f54e12e48b5a7cbf07f03706f4bad689d6
awaisabir <aqureshi@klipfolio.com>
2019-05-26 01:13:43 -0400

    added tags for collections

--- src/components/collections/Collection.js
+++ src/components/collections/Collection.js
@@ -1,48 +1,45 @@
 import React from "react";
-import StyledComponents from "styled-components";
-import { Image , Card } from "semantic-ui-react";
+import { Image , Card, Label } from "semantic-ui-react";
 
-const CollectionsContainer = StyledComponents.div`
-    justify-content: space-evenly;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: flex-end;
-`;
-
-class Collections extends React.Component {
-    renderCollections = () => (
-        this.props.collections.results.map((collection) => {
-            const url = collection.cover_photo ? collection.cover_photo.urls.regular : "https://imgplaceholder.com/420x320/ff7f7f/333333/fa-image";
-            
-            return (
-                <Card>
-                    <Image 
-                        src={url}
-                        size={"small"}
-                        style={{width: "100%"}}
-                    />
-                    <Card.Content>
-                        <Card.Header>{collection.title}</Card.Header>
-                        <Card.Meta>
-                            <span className='date'>Created in {new Date(collection.published_at).getFullYear()}</span>
-                        </Card.Meta>
-                        <Card.Description>
-                            {collection.description}
-                        </Card.Description>
-                    </Card.Content>
-                </Card>
-            );
-        })
-    );
-
+class Collection extends React.Component {
     render() {
-        return (
-            <CollectionsContainer>
-                {this.renderCollections()}
-            </CollectionsContainer>
+        const {
+            collection,
+            url
+        } = this.props;
+
+        const renderTags = () => {
+            if (collection.tags) {
+                return (
+                    collection.tags.map((tag, i) => (
+                        <Label key={i} style={{marginTop: "5px"}}>
+                            {tag.title}
+                        </Label>
+                    ))
+                );
+            }
+        };
+
+        return(
+            <Card>
+                <Image 
+                    src={url}
+                    size={"small"}
+                    style={{width: "100%"}}
+                />
+                <Card.Content>
+                    <Card.Header>{collection.title}</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>Created in {new Date(collection.published_at).getFullYear()}</span>
+                    </Card.Meta>
+                    {renderTags()}
+                    <Card.Description style={{marginTop: "5px"}}>
+                        {collection.description}
+                    </Card.Description>
+                </Card.Content>
+            </Card>
         );
     }
-}
+};
 
-export default Collections;
+export default Collection;
