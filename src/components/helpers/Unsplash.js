5725d62b2cd021e779350941861879067ddb2526
awaisabir <aqureshi@klipfolio.com>
2019-05-26 16:11:22 -0400

    changed to regular class

--- src/components/helpers/Unsplash.js
+++ src/components/helpers/Unsplash.js
@@ -1,54 +1,43 @@
 import Unsplash from "unsplash-js";
 import config from "../../config/Unsplash";
 
-class UnsplashSingleton {
+class UNSPLASH {
     constructor() {
-        if (!UnsplashSingleton.instance) {
-            this._unsplash = new Unsplash({
-                applicationId: config.ACCESS_KEY,
-                secret: config.SECRET_KEY
-            });
-        }
-
-        UnsplashSingleton.instance = this;
+        this._unsplash = new Unsplash({
+            applicationId: config.ACCESS_KEY,
+            secret: config.SECRET_KEY
+        });
     }
 
-    getPhotos = async (page, perPage = 20, orderBy = "latest") => {
+    async getPhotos(page, perPage = 20, orderBy = "latest") {
         const response = await this._unsplash.photos.listPhotos(page, perPage, orderBy);
         const photos = await response.json();
 
         return photos;
     };
 
-    getPhotoById = async (id) => {
+    async getPhotoById(id) {
         const response = await this._unsplash.photos.getPhoto(id);
         const photo = await response.json();
 
         return photo;
     };
     
-    getPhotosByTerm = async (term, page = 1, per_page = 20) => {
+    async getPhotosByTerm(term, page = 1, per_page = 20) {
         const response = await this._unsplash.search.photos(term, page, per_page);
         const photos = await response.json();
 
         return photos;
     };
 
-    getCollections = async (page, perPage = 20, orderBy = "latest") => {
-        const response = await this._unsplash.collections.listCollections(page, perPage, orderBy);
-        const collections = await response.json();
-
-        return collections;
-    };
-
-    getCollectionById = async (id) => {
+    async getCollectionById(id) {
         const response = await this._unsplash.collections.getCollection(id);
         const collection = await response.json();
 
         return collection;
     };
 
-    getCollectionsByTerm = async (term, page = 1, per_page = 20) => {
+    async getCollectionsByTerm(term, page = 1, per_page = 20) {
         const response = await this._unsplash.search.collections(term, page, per_page);
         const collections = await response.json();
 
@@ -56,6 +45,4 @@ class UnsplashSingleton {
     };
 };
 
-const instance = new UnsplashSingleton();
-
-export default instance;
+export default UNSPLASH;
